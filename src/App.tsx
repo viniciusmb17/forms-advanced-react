88002bce8c990cf00a2e3060ce3786eec0da413d
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-05-12 14:10:35 -0300

    feat: outputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import { useForm } from 'react-hook-form'
 import './styles/global.css'
+import { useState } from 'react'
 
 export function App() {
-  const { register } = useForm()
+  const [output, setOutput] = useState('')
+  const { register, handleSubmit } = useForm()
+
+  function createUser(data: any) {
+    setOutput(JSON.stringify(data, null, 2))
+  }
 
   return (
-    <main className="h-screen bg-zinc-950 text-zinc-300 flex flex-col items-center justify-center gap-4">
+    <main className="h-screen bg-zinc-950 text-zinc-300 flex flex-col items-center justify-center gap-10">
+      <form
+        className="flex flex-col gap-4 w-full max-w-xs"
+        onSubmit={handleSubmit(createUser)}
+      >
         <div className="flex flex-col gap-1">
           <label htmlFor="email">E-mail</label>
           <input
-            className="border bg-zinc-900 border-zinc-600 shadow-sm rounded h-10 px-3"
+            className="border bg-zinc-800 border-zinc-600 shadow-sm rounded h-10 px-3"
             type="email"
             {...register('email')}
           />
@@ -18,7 +28,7 @@ export function App() {
         <div className="flex flex-col gap-1">
           <label htmlFor="password">Senha</label>
           <input
-            className="border bg-zinc-900 border-zinc-600 shadow-sm rounded h-10 px-3"
+            className="border bg-zinc-800 border-zinc-600 shadow-sm rounded h-10 px-3"
             type="password"
             {...register('password')}
           />
@@ -31,6 +41,8 @@ export function App() {
           Salvar
         </button>
       </form>
+
+      {output && <pre>{output}</pre>}
     </main>
   )
 }
